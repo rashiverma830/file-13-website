5fb1fa64fe3496d4059d30dc2fa84b9f290171f1
unknown <verma.rashi0111@gmail.com>
2025-07-31 11:42:42 +0530

    updated code

--- script.js
+++ script.js
@@ -14,17 +14,15 @@ for (i = 0; i < acc.length; i++) {
     } 
   });
 }
-// var acc = document.getElementsByClassName("accordion");
-// var i;
 
-// for (i = 0; i < acc.length; i++) {
-//   acc[i].addEventListener("click", function() {
-//     this.classList.toggle("active");
-//     var panel = this.nextElementSibling;
-//     if (panel.style.display === "block") {
-//       panel.style.display = "none";
-//     } else {
-//       panel.style.display = "block";
-//     }
-//   });
-// }
+
+function myFunction() {
+  const x = document.getElementById("myTopnav");
+
+  if (x.style.display === "flex") {
+    x.style.display = "none"; // Hide
+  } else {
+    x.style.display = "flex"; // Show
+  }
+}
+
